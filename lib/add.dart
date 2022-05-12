649ffa92acbb2f45bb155683c0740cfc5e5cbc02
thitimon171143 <montha.kam.iam@gmail.com>
2022-05-12 12:03:55 +0700

    commit

---
+++ lib/add.dart
@@ -0,0 +1,144 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'database.dart';
+
+class Add extends StatefulWidget {
+  Add({Key key, this.db}) : super(key: key);
+  Database db;
+  @override
+  _AddState createState() => _AddState();
+}
+
+class _AddState extends State<Add> {
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController weightController = new TextEditingController();
+  TextEditingController heightController = new TextEditingController();
+  TextEditingController ageController = new TextEditingController();
+  TextEditingController sexController = new TextEditingController();
+  TextEditingController resultController = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff00BCD1),
+      appBar: AppBar(
+        //backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+        title: Text("BMI Add"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                // widget.db.delete(widget.BMI["id"]);
+                // Navigator.pop(context, true);
+              })
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Name"),
+                controller: nameController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Weight"),
+                controller: weightController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Height"),
+                controller: heightController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Age"),
+                controller: ageController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Sex"),
+                controller: sexController,
+              ),
+              /*SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Result"),
+                controller: resultController,
+              ),*/
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        color: Colors.transparent,
+        child: BottomAppBar(
+          color: Colors.transparent,
+          child: OutlinedButton(
+              //color: Colors.black,
+              onPressed: () {
+                widget.db.create(
+                  nameController.text,
+                  weightController.text,
+                  heightController.text,
+                  ageController.text,
+                  sexController.text,
+                  resultController.text,
+
+                  
+                );
+                Navigator.pop(context, true);
+              },
+              child: Text(
+                "Calculate",
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(String labelText) {
+    return InputDecoration(
+      focusColor: Colors.white,
+      labelStyle: TextStyle(color: Colors.white),
+      labelText: labelText,
+      fillColor: Colors.white,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 2.0,
+        ),
+      ),
+    );
+  }
+}
